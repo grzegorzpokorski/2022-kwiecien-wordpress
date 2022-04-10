6ac3afac487d8b473a8f2d1c85650f00d68c6722
unknown <mr.pokorski@gmail.com>
2022-04-10 14:08:14 +0200

    reorder footer items and add social media links in footer

--- footer.php
+++ footer.php
@@ -16,21 +16,8 @@ if( have_rows('logo', 'general_setting') ) {
 
 	<footer class="bg-white text-gray-light py-5 border-top border-1">
 		<div class="container">
-			<div class="row d-flex flex-column flex-md-row align-items-center gap-3 text-center">
-				<div class="col text-md-start">
-					<a href="<?php echo home_url(); ?>">
-						<h3 class="mb-0">
-
-						<?php if( !empty($first_part) && !empty($second_part) ): ?>
-							<span class="<?php echo esc_attr($first_classes); ?>"><?php echo $first_part; ?></span><span class="<?php echo esc_attr($second_classes); ?>"><?php echo $second_part; ?></span>
-						<?php else: ?>
-							<span class="fw-bold"><?php echo bloginfo('name'); ?></span>
-						<?php endif; ?>
-
-						</h3>
-					</a>
-				</div>
-				<nav class="col">
+			<div class="row d-flex flex-column flex-md-row align-items-center gap-2 text-center">
+				<nav class="col order-2 order-md-1">
 
 					<?php 
 
@@ -48,7 +35,50 @@ if( have_rows('logo', 'general_setting') ) {
 					?>
 
 				</nav>
-				<div class="col text-md-end">
+				<div class="col order-1 order-md-2">
+					<a href="<?php echo home_url(); ?>">
+						<h3 class="mb-0">
+
+						<?php if( !empty($first_part) && !empty($second_part) ): ?>
+							<span class="<?php echo esc_attr($first_classes); ?>"><?php echo $first_part; ?></span><span class="<?php echo esc_attr($second_classes); ?>"><?php echo $second_part; ?></span>
+						<?php else: ?>
+							<span class="fw-bold"><?php echo bloginfo('name'); ?></span>
+						<?php endif; ?>
+
+						</h3>
+					</a>
+				</div>
+
+				<?php if( have_rows('footer_social_items', 'general_setting') and get_field('footer_display_social_media', 'general_setting') ): ?>
+
+				<div class="col order-4 order-md-3">
+					<ul class="unstyled-list mb-0 d-flex flex-row flex-wrap gap-1 justify-content-center justify-content-md-end">
+
+						<?php while( have_rows('footer_social_items', 'general_setting') ): the_row(); ?>
+
+						<li>
+							<a href="<?php echo esc_url( get_sub_field('link') ); ?>">
+								<span class="sr-only">
+									<?php echo get_sub_field('label'); ?>
+								</span>
+								<span class="fa <?php echo esc_attr( get_sub_field('icon') ); ?>" aria-hidden="true"></span>
+							</a>
+						</li>
+
+						<?php endwhile; ?>
+						
+					</ul>
+				</div>
+
+				<div class="col-12 order-3 order-md-4">
+
+				<?php else: ?>
+
+				<div class="col order-3 order-md-4 text-md-end">
+
+				<?php endif; ?>
+
+		
 					<p>
 						<?php $footer_content = get_field('content', 'general_setting'); ?>
 						<?php echo bloginfo('name'); ?> &copy; <?php echo Date('Y'); ?>. <?php echo ($footer_content) ? $footer_content : ''; ?>
